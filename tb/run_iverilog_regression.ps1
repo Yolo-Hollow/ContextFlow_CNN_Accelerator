2ab9aeec9bcb57739da374cd99ddbfce2196a65b
Yolo <gem0816@iag.com>
2026-05-24 13:02:25 +0800

    Add small layer scheduler regression

--- tb/run_iverilog_regression.ps1
+++ tb/run_iverilog_regression.ps1
@@ -25,6 +25,7 @@ $tests = @(
     @{ Top = "tb_systolic_array_small"; Files = @("tb/tb_systolic_array_small.v") },
     @{ Top = "tb_systolic_top_multipass"; Files = @("tb/tb_systolic_top_multipass.v") },
     @{ Top = "tb_window_top_singlepass"; Files = @("tb/tb_window_top_singlepass.v") },
+    @{ Top = "tb_layer_scheduler_small"; Files = @("tb/tb_layer_scheduler_small.v") },
     @{ Top = "tb_window_extract"; Files = @("tb/tb_window_extract.v") },
     @{ Top = "tb_requant"; Files = @("tb/tb_requant.v") }
 )
